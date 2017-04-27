9409ee79dde23411aff7d2efb82328d7f23b6f72
adir zoari <adir1991@gmail.com>
2017-04-27 22:20:59 +0300

    changes - adir

---
+++ Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace irobot_simulation
+{
+    public partial class simulation : Form
+    {
+
+        int Lshort = 0, Llong = 0;
+        double Vd = 0, Vr = 0, R = 0, pi = 3.14;
+
+        private void label10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label12_Click(object sender, EventArgs e)
+        {
+
+        }
+
+      
+
+        public simulation()
+        {
+            InitializeComponent();
+
+          
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox5_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            Llong = int.Parse(Llong_text.Text);
+            Lshort = int.Parse(Lshort_text.Text);
+            Double.TryParse(vd_text.Text, out Vd);
+            Double.TryParse(vr_text.Text, out Vr);
+            Double.TryParse(r_text.Text, out R);
+            new_CCPN(Lshort, Llong, Vd, Vr, R, pi);
+            back_and_forth(Lshort, Llong, Vd, Vr, R, pi);
+            boundary_sweep(Lshort, Llong, Vd, Vr, R, pi);
+
+            
+
+
+
+
+
+        }
+
+        private void new_CCPN(int Lshort,int Llong,double Vd,double Vr,double R,double pi)
+        {
+            double new_Cclean = 1 - (((2 * R * R * (4 - pi) * ((int)(Lshort / R))) / (16 * Lshort * Llong)));
+            double new_CTlong = (((((Lshort - R) / R) * (Llong - 2 * R)) + R) / Vd);
+            double new_CTshort = 2 * ((Lshort - R) / Vd);
+            double new_CTrotation = 90 * 2 * ((((Lshort - R) / R) - 1) / Vr);
+            double new_CTotal = new_CTlong + new_CTshort + new_CTrotation;
+
+
+            cp_box.Text = new_Cclean.ToString();
+            cp_time.Text = new_CTotal.ToString();
+
+        }
+
+        private void back_and_forth(int Lshort, int Llong, double Vd, double Vr, double R, double pi)
+        {
+            double bnf_Cclean = 1 - ((2 * R * R * (4 - pi) * ((int)(Lshort / R) + 1)) / (16 * Lshort * Llong));
+            double bnf_Tlong = (((Llong - R) * (Lshort / R)) / Vd);
+            double bnf_Tshort = ((Lshort - R) / Vd);
+            double bnf_Trotation = 90 * 2 * ((Lshort / R) / Vr);
+            double bnf_Ttotal = bnf_Tlong + bnf_Tshort + bnf_Trotation;
+
+            t1.Text = bnf_Tlong.ToString();
+            t2.Text = bnf_Tshort.ToString();
+            t3.Text = bnf_Trotation.ToString();
+            bnf_box.Text = bnf_Cclean.ToString();
+            bf_time.Text = bnf_Ttotal.ToString();
+
+        }
+
+
+
+        private void boundary_sweep(int Lshort, int Llong, double Vd, double Vr, double R, double pi)
+        {
+
+            double boundry_Cclean = 1 - (((R * R * (4 - pi) * ((2 * (Lshort - R) + 2) / R) / (16 * Lshort * Llong))));
+            double alpha = (int)((Lshort - R) / R) + 1;
+            double boundry_Tlong = alpha * ((2 * (Llong - R * alpha) + R * (alpha - 1)) / Vd);
+            double boundry_Tshort = alpha * ((2 * (Lshort - R * alpha) + R * (alpha - 1)) / Vd);
+            double boundry_Trotation = ((90 * 2 * alpha) / Vr);
+            double boundry_Treturn = Math.Sqrt(((Math.Pow(((Llong - R) / (2)), 2) + (Math.Pow((Lshort - R) / 2, 2)) / Vd)));
+            double bounday_Ttotal = boundry_Treturn + boundry_Tshort + boundry_Tlong;
+
+            bs_box.Text = boundry_Cclean.ToString();
+            bs_time.Text = bounday_Ttotal.ToString();
+
+        }
+
+
+
+
+
+
+
+    }
+}
--- irobot simulation/irobot simulation/Form1.cs
+++ irobot simulation/irobot simulation/Form1.cs
@@ -12,39 +12,144 @@ namespace irobot_simulation
 {
     public partial class simulation : Form
     {
+
+        int Lshort = 0, Llong = 0;
+        double Vd = 0.0, Vr = 0, R = 0, pi = 3.14;
+
+        double TotalTime_CCPP_Set = 0.0, TotalTime_backNforth_Set = 0.0,TotalTime_boundarySweep_Set= 0.0; 
+        double TotalCoverage_CCPP_Set = 0.0, TotalCoverage_backNforth_Set= 0.0, TotalCoverage_boundarySweep_Set= 0.0;
         
+        //temp local variables
+        double temp_CCPP_TotalTime = 0.0, temp_CCPP_Coverage = 0.0;
+        double temp_backNforth_Coverage = 0.0, temp_backNforth_TotalTime = 0.0;
+        double  temp_boundarySweep_Coverage = 0.0, temp_boundarySweep_TotalTime = 0.0;
+
 
         public simulation()
         {
             InitializeComponent();
 
-            int Lshort=0, Llong=0;
-            double Vd=0, Vr=0, R=0, pi = 3.14;
+          
+        }
 
-            double new_Cclean = 1 - (((2 * R * R * (4 - pi) * ((int)(Lshort / R) + 1)) / 16 * Lshort * Llong));
-            double boundry_Cclean = 1 - (((R * R * (4 - pi) * ((2 * (Lshort - R) + 2) / R) / (16 * Lshort * Llong))));
-            double bnf_Cclean = 1 - ((2 * R * R * (4 - pi) * ((int)(Lshort / R) + 1)) / (16 * Lshort * Llong));
+      
+       
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dgv.Rows.Clear();
+            testdgv.Rows.Clear();
+            dgv.Refresh();
+            testdgv.Rows.Clear();
 
+            // intialize the variables
+            TotalTime_CCPP_Set = 0.0;
+            TotalTime_backNforth_Set = 0.0;
+            TotalTime_boundarySweep_Set = 0.0;
+            TotalCoverage_CCPP_Set = 0.0;
+            TotalCoverage_backNforth_Set = 0.0;
+            TotalCoverage_boundarySweep_Set = 0.0;
+          
+            for (int j = 0; j < 10; j++)
+            {
+                //intialize the variables each set
+                TotalTime_CCPP_Set = 0.0;
+                TotalTime_backNforth_Set = 0.0;
+                TotalTime_boundarySweep_Set = 0.0;
+                TotalCoverage_CCPP_Set = 0.0;
+                TotalCoverage_backNforth_Set = 0.0;
+                TotalCoverage_boundarySweep_Set = 0.0;
+
+                
+                for (int i = 0; i < 1000; i++)
+                {
+                    // randome numbers
+                    var rnd = new Random(Guid.NewGuid().GetHashCode());
+                    Lshort = rnd.Next(5, 51);
+                    Llong = rnd.Next(10, 151);
+                    Vd = rnd.NextDouble() * (0.50 - 0.28) + 0.28;
+                    Vr = rnd.Next(75, 136);
+                    R = rnd.NextDouble() * (0.50 - 0.40) + 0.40;
+
+                    testdgv.Rows.Add(i,Lshort, Llong, Vr, Vd, R);
+
+                    // call to functions to calculate
+                    CCPP_Func(Lshort, Llong, Vd, Vr, R, pi);
+                    backNforth_Func(Lshort, Llong, Vd, Vr, R, pi);
+                    boundarySweep_Func(Lshort, Llong, Vd, Vr, R, pi);
+                    dgv2.Rows.Add(i, Math.Round(temp_backNforth_TotalTime, 1), Math.Round(temp_boundarySweep_TotalTime , 1), Math.Round(temp_CCPP_TotalTime , 1));
+
+
+                }
+
+
+               
+
+                dgv.Rows.Add(j+1, Math.Round((TotalTime_backNforth_Set / 1000),1), Math.Round((TotalTime_boundarySweep_Set / 1000),1),Math.Round((TotalTime_CCPP_Set / 1000),1));
+
+            }
+
+
+       
+        }
+
+        private void CCPP_Func(int Lshort,int Llong,double Vd,double Vr,double R,double pi)
+        {
+            
+            temp_CCPP_Coverage = 1 - (((2 * R * R * (4 - pi) * ((int)(Lshort / R))) / (16 * Lshort * Llong)));
+            double new_CTlong = (((((Lshort - R) / R) * (Llong - 2 * R)) + R) / Vd);
+            double new_CTshort = 2 * ((Lshort - R) / Vd);
+            double new_CTrotation = 90 * 2 * ((((Lshort - R) / R) - 1) / Vr);
+            temp_CCPP_TotalTime = new_CTlong + new_CTshort + new_CTrotation;
+
+
+            TotalCoverage_CCPP_Set += temp_CCPP_Coverage;
+            TotalTime_CCPP_Set += temp_CCPP_TotalTime;
+
+
+
+        }
+
+        private void backNforth_Func(int Lshort, int Llong, double Vd, double Vr, double R, double pi)
+        {
+            temp_backNforth_Coverage = 1 - ((2 * R * R * (4 - pi) * ((int)(Lshort / R) + 1)) / (16 * Lshort * Llong));
             double bnf_Tlong = (((Llong - R) * (Lshort / R)) / Vd);
             double bnf_Tshort = ((Lshort - R) / Vd);
             double bnf_Trotation = 90 * 2 * ((Lshort / R) / Vr);
-            double bnf_Ttotal = bnf_Tlong + bnf_Tshort + bnf_Trotation;
+            temp_backNforth_TotalTime = bnf_Tlong + bnf_Tshort + bnf_Trotation;
 
-            double alpha = (int)((Lshort - R) / R) + 1;
+       
+            TotalTime_backNforth_Set += temp_backNforth_TotalTime;
+            TotalCoverage_backNforth_Set += temp_backNforth_Coverage;
+         
+
+        }
+
+
+
+        private void boundarySweep_Func(int Lshort, int Llong, double Vd, double Vr, double R, double pi)
+        {
+
+            temp_boundarySweep_Coverage = 1 - (((R * R * (4 - pi) * ((2 * (Lshort - R) + 2) / R) / (16 * Lshort * Llong))));
+
+            double alpha = (int)((Lshort - R) / R);
             double boundry_Tlong = alpha * ((2 * (Llong - R * alpha) + R * (alpha - 1)) / Vd);
             double boundry_Tshort = alpha * ((2 * (Lshort - R * alpha) + R * (alpha - 1)) / Vd);
             double boundry_Trotation = ((90 * 2 * alpha) / Vr);
             double boundry_Treturn = Math.Sqrt(((Math.Pow(((Llong - R) / (2)), 2) + (Math.Pow((Lshort - R) / 2, 2)) / Vd)));
-        }
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
+            temp_boundarySweep_TotalTime = boundry_Trotation + boundry_Tshort + boundry_Tlong;
 
-        }
-
-        private void comboBox5_SelectedIndexChanged(object sender, EventArgs e)
-        {
+            TotalCoverage_boundarySweep_Set += temp_boundarySweep_Coverage;
+            TotalTime_boundarySweep_Set += temp_boundarySweep_TotalTime;
+          
 
         }
+
+
+
+
+
+
+
     }
 }
